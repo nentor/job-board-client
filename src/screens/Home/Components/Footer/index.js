51f0e2db579152bab566aa6794f2510eff5b3d36
Ablax <ablax@ablax.me>
2020-11-26 22:21:09 +0200

    Revert "nishto novo"
    This reverts commit f1c66bb744847d69a25cbc0c93502761f5e0294b.

--- src/screens/Home/Components/Footer/index.js
+++ src/screens/Home/Components/Footer/index.js
@@ -1,18 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
 
-
-const Component = styled.div`
-  color: red;
-`;
-
-render(
-  <Component
-    as="button"
-    onClick={() => alert('It works!')}
-  >
-    Hello World!
-  </Component>
-)
-
-export default Footer;
+export const Footer = () => {
+  return <div>Footer</div>
+}
