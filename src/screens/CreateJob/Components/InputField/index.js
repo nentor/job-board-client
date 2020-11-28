3d8481148b7e69d6a9c426e0c047014f3d0f531a
nentor75 <nentormail@gmail.com>
2020-11-28 21:48:54 +0200

    Improve styling for the InputField component

--- src/screens/CreateJob/Components/InputField/index.js
+++ src/screens/CreateJob/Components/InputField/index.js
@@ -3,19 +3,35 @@ import styled from 'styled-components'
 
 import { RequiredCircle } from '../index'
 
+const Container = styled.div`
+  margin-top: 20px;
+`
+
 const Input = styled.input`
   max-width: 890px;
   width: 890px;
+  margin: 5px 0 3px;
+  font-size: 0.95rem;
+  padding: 16px 12px 16px 12px;
+  cursor: auto;
+  border-color: rgba(144, 146, 148, 0.2);
+  border-style: solid;
+  border-radius: 3px;
+  font-family: 'Neufile Grotesk Regular', sans-serif;
+  margin: 0 0 5px 0;
+  background-color: #f8f9fa;
+  outline-width: 2px;
+  outline-color: rgba(144, 146, 148, 0.2);
 `
 
 export const InputField = ({ type, title, inputText, isRequired }) => {
   return (
-    <>
+    <Container>
       <h3>
         {title} {isRequired && <RequiredCircle />}
       </h3>
-      <input type={type} />
+      <Input type={type} />
       <p>{inputText}</p>
-    </>
+    </Container>
   )
 }
