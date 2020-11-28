4e6cff8b26196461ebe2a9ae3edbb38a5980135b
nentor75 <nentormail@gmail.com>
2020-11-28 17:22:18 +0200

    Import all components into the CreateJob index.js

--- src/screens/CreateJob/index.js
+++ src/screens/CreateJob/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import {
+  InputField,
+  NewListing,
+  RequiredCircle,
+  TextEditor,
+  Title,
+} from './Components'
+
 export const CreateJob = () => {
   return <div></div>
 }
