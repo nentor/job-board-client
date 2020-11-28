b48b93e20e20578e5dd3f2f4bfa78f84d0aafa65
nentor75 <nentormail@gmail.com>
2020-11-28 21:23:21 +0200

    Add 30px padding to the NewListing wrapper component

--- src/screens/CreateJob/Components/NewListing/index.js
+++ src/screens/CreateJob/Components/NewListing/index.js
@@ -5,6 +5,7 @@ const Wrapper = styled.section`
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
   border-radius: 4px;
   background: #fff;
+  padding: 30px;
 `
 
 export const NewListing = ({ children }) => {
