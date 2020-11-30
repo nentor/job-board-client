dc7a13c7f7ca5acc18b67352c481597f67b557bb
nentor75 <nentormail@gmail.com>
2020-11-30 19:32:53 +0200

    Add styling to SelectField component

--- src/screens/CreateJob/Components/SelectField/index.js
+++ src/screens/CreateJob/Components/SelectField/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Select = styled.select`
+  font-size: 0.8em;
+  outline: none;
+  width: 435px;
+  height: 60px;
+  border: 1.5px solid rgba(144, 146, 148, 0.2);
+`
 
 export const SelectField = () => {
-  return <div></div>
+  return <Select></Select>
 }
