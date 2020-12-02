99425d10b4742b7d32fab6f04d4af5cfaefcb21c
nentor75 <nentormail@gmail.com>
2020-12-02 21:53:24 +0200

    Export Container into its own component

---
+++ src/screens/CreateJob/Components/Container/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin-top: 30px;
+
+  & label {
+    font-weight: 900;
+    color: #212529;
+    font-size: 1.1rem;
+    display: block;
+  }
+`
+
+export default Container
