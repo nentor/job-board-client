466d88b30c06aa5f516a6903093916cda6626d1d
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-12-08 21:01:31 +0200

    setup temp height on container to simulate content

---
+++ src/screens/Home/Components/Body/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 52vh;
+`
+
+export const Body = () => {
+  return <Container>Body</Container>
+}
