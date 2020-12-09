6e22c2b43f745e25df965bc1a7a1be0f63effcf0
nentor75 <nentormail@gmail.com>
2020-12-09 09:38:09 +0200

    Remove label styling from the Container component

--- src/screens/CreateJob/Components/Container/index.js
+++ src/screens/CreateJob/Components/Container/index.js
@@ -3,11 +3,4 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   margin-top: 30px;
-
-  & label {
-    font-weight: 900;
-    color: #212529;
-    font-size: 1.1rem;
-    display: block;
-  }
 `
