02516b5836ab6ad6ed307686de3f10fe1e866cbb
nentor75 <nentormail@gmail.com>
2020-12-09 09:20:17 +0200

    Create a new component ListingTitle

---
+++ src/screens/CreateJob/Components/ListingTitle/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Label = styled.label`
+  font-weight: 900;
+  color: #212529;
+  font-size: 1.1rem;
+  display: block;
+`
+
+export const ListingTitle = ({ name }) => {
+  return <Label htmlFor={name}>{name}</Label>
+}
