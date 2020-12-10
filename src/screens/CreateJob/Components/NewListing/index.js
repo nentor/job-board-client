0e26d7067febb47cc5e24b282dc16298ec083f8b
nentor75 <nentormail@gmail.com>
2020-12-10 23:42:12 +0200

    Change padding values on the NewListing component

--- src/screens/CreateJob/Components/NewListing/index.js
+++ src/screens/CreateJob/Components/NewListing/index.js
@@ -5,7 +5,8 @@ const Wrapper = styled.section`
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
   border-radius: 4px;
   background: #fff;
-  padding: 30px;
+  /* padding: 30px; */
+  padding: 10px 30px;
 `
 
 export const NewListing = ({ children }) => {
