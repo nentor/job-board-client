67156a8672c090b1376571fd8d689dc038dca93a
nentor75 <nentormail@gmail.com>
2020-12-10 23:39:26 +0200

    Add padding to Title component

--- src/screens/CreateJob/Components/Title/index.js
+++ src/screens/CreateJob/Components/Title/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 export const Title = styled.h2`
+  padding: 30px 0px 15px 0px;
   font-weight: bold;
   font-size: 1.5rem;
   color: #f03e3e;
