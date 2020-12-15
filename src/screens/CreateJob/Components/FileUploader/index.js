037826a9ba595ce5e41a025520794c23afa509a4
nentor75 <nentormail@gmail.com>
2020-12-15 19:25:45 +0200

    Fix styling for file uploader and add text inside of it indicating its purpose

--- src/screens/CreateJob/Components/FileUploader/index.js
+++ src/screens/CreateJob/Components/FileUploader/index.js
@@ -7,8 +7,8 @@ const InputFileUpload = styled.input.attrs({
   type: 'file',
 })`
   opacity: 0;
-  /* min-height: 14rem; */
-  /* min-width: 88.2rem; */
+  min-height: 14rem;
+  min-width: 88.2rem;
   cursor: pointer;
 `
 
@@ -23,6 +23,19 @@ const InputFileUploadWrapper = styled.div`
   background-color: #f8f9fa;
   outline-width: 2px;
   outline-color: rgba(144, 146, 148, 0.2);
+
+  position: relative;
+
+  &::after {
+    content: 'Click or drag your photo here to upload';
+    display: inline-block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 1.75rem;
+    pointer-events: none;
+  }
 `
 
 export const FileUploader = ({ title, isRequired, inputText }) => {
