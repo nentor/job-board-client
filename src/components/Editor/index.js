142b5bc7028f9693a944a65e4a52b0eab7b2bf35
nentor75 <nentormail@gmail.com>
2020-12-25 14:38:19 +0200

    Remove unnecessary h2 from Editor component

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
-import CKEditor from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import React, { Component } from 'react'
+import CKEditor from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 // https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/react.html
 class Editor extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Using CKEditor 5 build in React</h2>
         <CKEditor
           editor={ClassicEditor}
           // https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/react.html#component-properties
@@ -17,37 +16,37 @@ class Editor extends Component {
             // https://ckeditor.com/docs/ckeditor5/latest/features/image-upload/simple-upload-adapter.html
             simpleUpload: {
               // The URL that the images are uploaded to.
-              uploadUrl: "http://example.com",
+              uploadUrl: 'http://example.com',
 
               // Enable the XMLHttpRequest.withCredentials property.
               withCredentials: true,
 
               // Headers sent along with the XMLHttpRequest to the upload server.
               headers: {
-                "X-CSRF-TOKEN": "CSRF-Token",
-                Authorization: "Bearer <JSON Web Token>",
+                'X-CSRF-TOKEN': 'CSRF-Token',
+                Authorization: 'Bearer <JSON Web Token>',
               },
             },
           }}
           data="<p>Hello from CKEditor 5!</p>"
           onInit={(editor) => {
             // You can store the "editor" and use when it is needed.
-            console.log("Editor is ready to use!", editor);
+            console.log('Editor is ready to use!', editor)
           }}
           onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
+            const data = editor.getData()
+            console.log({ event, editor, data })
           }}
           onBlur={(event, editor) => {
-            console.log("Blur.", editor);
+            console.log('Blur.', editor)
           }}
           onFocus={(event, editor) => {
-            console.log("Focus.", editor);
+            console.log('Focus.', editor)
           }}
         />
       </div>
-    );
+    )
   }
 }
 
-export default Editor;
+export default Editor
