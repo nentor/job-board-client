abeab580d84ec6a7582cc692af46b6fa47162e3d
Ablax <ablax@ablax.me>
2020-12-26 17:38:33 +0200

    Reformat and add id's to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,95 +1,88 @@
 import React from 'react'
 import styled from 'styled-components'
-import {DesktopFooter} from './Components'
-import {MobileFooter} from './Components'
+import { DesktopFooter } from './Components'
+import { MobileFooter } from './Components'
 
-export const Footer = ({data}) => {
-
+export const Footer = ({ data }) => {
   const ResourcesBannerData = [
-    "Top 100 Companies",
-      "All Remote Companies",
-      "WWR FAQ",
-      "Newsletter",
-      "Blog",
-      "Learning Portal",
-      "Forum",
-      "RSS",
-    ]
-  
-    const BannerResourcesData = [
-      "Copyright © 2020",
-      "Terms",
-      "Privacy",
-      "Guidelines"
-    ]
-  
-    const MobileFooterData = [
-      "Top 100 Companies",
-      "All Remote Companies",
-      "WWR FAQ",
-      "Newsletter",
-      "Blog",
-      "Learning Portal",
-      "Forum",
-      "RSS",
-      "Terms",
-      "Privacy",
-      "Guidelines"
-    ]
+    { id: 1, label: 'Top 100 Companies' },
+    { id: 2, label: 'All Remote Companies' },
+    { id: 3, label: 'WWR FAQ' },
+    { id: 4, label: 'Newsletter' },
+    { id: 5, label: 'Blog' },
+    { id: 6, label: 'Learning Portal' },
+    { id: 7, label: 'Forum' },
+    { id: 8, label: 'RSS' },
+  ]
 
-    const footerData = {
-      name: "We work remotely",
-      anchor: "Get in touch",
-      number: "19 000",
-      job: "Remote jobs posted",
-      button: "Post a job",
-      bottom: "Back to top ↑" ,
-      text: "We strongly encourage employers to embrace diversity, equity, and inclusion as fundamental values when hiring through We Work Remotely."
-    }
+  const BannerResourcesData = [
+    { id: 1, label: 'Copyright © 2020' },
+    { id: 2, label: 'Terms' },
+    { id: 3, label: 'Privacy' },
+    { id: 4, label: 'Guidelines' },
+  ]
 
+  const MobileFooterData = [
+    { id: 1, label: 'Top 100 Companies' },
+    { id: 2, label: 'All Remote Companies' },
+    { id: 3, label: 'WWR FAQ' },
+    { id: 4, label: 'Newsletter' },
+    { id: 5, label: 'Blog' },
+    { id: 6, label: 'Learning Portal' },
+    { id: 7, label: 'Forum' },
+    { id: 8, label: 'RSS' },
+    { id: 9, label: 'Terms' },
+    { id: 10, label: 'Privacy' },
+    { id: 11, label: 'Guidelines' },
+  ]
 
-  
-const DataMobile = {
-  total: "15,511",
-  span: "Job Posted",
-  copy: " Copyright © 2020 "
+  const footerData = {
+    name: 'We work remotely',
+    anchor: 'Get in touch',
+    number: '19 000',
+    job: 'Remote jobs posted',
+    button: 'Post a job',
+    bottom: 'Back to top ↑',
+    text:
+      'We strongly encourage employers to embrace diversity, equity, and inclusion as fundamental values when hiring through We Work Remotely.',
+  }
 
-}
-
+  const DataMobile = {
+    total: '15,511',
+    span: 'Job Posted',
+    copy: ' Copyright © 2020 ',
+  }
 
   return (
-<div>
-<FooterContainer>
-<DesktopFooter ResourcesBannerData = {ResourcesBannerData}  BannerResourcesData = {BannerResourcesData } 
-footerData = {footerData}/>
+    <div>
+      <FooterContainer>
+        <DesktopFooter
+          ResourcesBannerData={ResourcesBannerData}
+          BannerResourcesData={BannerResourcesData}
+          footerData={footerData}
+        />
 
-
-  <MobileFooter MobileFooterData = {MobileFooterData} 
-  DataMobile = {DataMobile}>
-</MobileFooter>
-
-</FooterContainer>
-
-
-
-</div>
- )
- }
+        <MobileFooter
+          MobileFooterData={MobileFooterData}
+          DataMobile={DataMobile}
+        ></MobileFooter>
+      </FooterContainer>
+    </div>
+  )
+}
 
 export default Footer
 
 const FooterContainer = styled.div`
-text-size-adjust: 100%;
-color: #9b9b9b;
-background: #212529;
-overflow: hidden;
-width: 100vw;
-flex-direction: column;
+  text-size-adjust: 100%;
+  color: #9b9b9b;
+  background: #212529;
+  overflow: hidden;
+  width: 100vw;
+  flex-direction: column;
 
-@media (max-width: 600px) {
-  text-align: center;
-  padding-bottom: 10px;
-}
-`;
-
-
+  @media (max-width: 600px) {
+    text-align: center;
+    padding-bottom: 10px;
+  }
+`
