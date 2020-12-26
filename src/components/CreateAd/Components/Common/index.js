562cbf08b7ab8c72f1c8824339a27ef48a87d34b
NentoR <nentormail@gmail.com>
2020-12-26 12:09:01 +0200

    Move Title code to Common

--- src/components/CreateAd/Components/Common/index.js
+++ src/components/CreateAd/Components/Common/index.js
@@ -23,3 +23,10 @@ const LabelWrapper = styled.label`
 export const Label = ({ name, children }) => {
   return <LabelWrapper htmlFor={name}>{children}</LabelWrapper>
 }
+
+export const Title = styled.h2`
+  padding: 3rem 0 1.5rem 0;
+  font-weight: bold;
+  font-size: 2.4rem;
+  color: #f03e3e;
+`
