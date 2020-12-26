8af33b99ba7f8a138538b8e30e3b4df91cde9b1c
NentoR <nentormail@gmail.com>
2020-12-26 12:05:48 +0200

    Move Label component code to Common

--- src/components/CreateAd/Components/Common/index.js
+++ src/components/CreateAd/Components/Common/index.js
@@ -11,3 +11,15 @@ export const Container = styled.div`
     font-size: 1.408rem;
   }
 `
+
+const LabelWrapper = styled.label`
+  font-weight: 900;
+  color: #212529;
+  font-size: 1.76rem;
+  display: block;
+  margin-bottom: 0.6rem;
+`
+
+export const Label = ({ name, children }) => {
+  return <LabelWrapper htmlFor={name}>{children}</LabelWrapper>
+}
