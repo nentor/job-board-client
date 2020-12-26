acc1ca323f9f9e02d163fce3f256cba50bbf0cca
NentoR <nentormail@gmail.com>
2020-12-26 12:10:24 +0200

    Move RequiredIndicator code to Common

--- src/components/CreateAd/Components/Common/index.js
+++ src/components/CreateAd/Components/Common/index.js
@@ -30,3 +30,17 @@ export const Title = styled.h2`
   font-size: 2.4rem;
   color: #f03e3e;
 `
+
+export const RequiredIndicator = styled.span`
+  &::before {
+    content: '';
+    background: #f03e3e;
+    display: inline-block;
+    width: 0.9rem;
+    height: 0.9rem;
+    border-radius: 50%;
+    position: relative;
+    top: -0.2rem;
+    left: 0.4rem;
+  }
+`
