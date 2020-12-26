6f69d05c43257b596a35b328e4767348704d02eb
NentoR <nentormail@gmail.com>
2020-12-26 16:09:38 +0200

    Comment out on-key events in Editor

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -29,20 +29,20 @@ class Editor extends Component {
             },
           }}
           data=""
-          onInit={(editor) => {
-            // You can store the "editor" and use when it is needed.
-            console.log('Editor is ready to use!', editor)
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData()
-            console.log({ event, editor, data })
-          }}
-          onBlur={(event, editor) => {
-            console.log('Blur.', editor)
-          }}
-          onFocus={(event, editor) => {
-            console.log('Focus.', editor)
-          }}
+          // onInit={(editor) => {
+          //   // You can store the "editor" and use when it is needed.
+          //   console.log('Editor is ready to use!', editor)
+          // }}
+          // onChange={(event, editor) => {
+          //   const data = editor.getData()
+          //   console.log({ event, editor, data })
+          // }}
+          // onBlur={(event, editor) => {
+          //   console.log('Blur.', editor)
+          // }}
+          // onFocus={(event, editor) => {
+          //   console.log('Focus.', editor)
+          // }}
         />
       </div>
     )
