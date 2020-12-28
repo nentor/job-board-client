051e11e61eaba07c5e2621931dbb49b8352ced5a
NentoR <nentormail@gmail.com>
2020-12-28 13:22:42 +0200

    Add rendering logic of components based on hardcoded piece of data

--- src/components/CreateAd/index.js
+++ src/components/CreateAd/index.js
@@ -18,22 +18,6 @@ import { Reset } from '../../components/Reset'
 import { useForm } from '../../hooks/useForm'
 
 export const CreateAd = () => {
-  const { formState, setFormState, handleChange, handleSubmit } = useForm({
-    jobTitle: '',
-    category: '',
-    jobType: 'Full-Time',
-    companyHQ: '',
-    regionalRestrictions: '',
-    howToApply: '',
-    jobDescription: '',
-    companyName: '',
-    companyStatement: '',
-    logo: '',
-    companyWebsiteURL: '',
-    email: '',
-    companyDescription: '',
-  })
-
   const data = [
     {
       children: [
@@ -43,6 +27,7 @@ export const CreateAd = () => {
           meta: {
             description: 'Job description',
             header: 'Job Title',
+            name: 'jobTitle',
           },
         },
       ],
@@ -55,6 +40,7 @@ export const CreateAd = () => {
           meta: {
             description: 'Job description',
             header: 'Job Title',
+            name: 'category',
           },
           children: [
             'gosho',
@@ -72,8 +58,9 @@ export const CreateAd = () => {
           meta: {
             description: 'Job description',
             header: 'Job Title',
+            name: 'jobType',
           },
-          children: ['gosho', 'gosho'],
+          children: ['gosho', 'tosho', 'dancho'],
         },
       ],
     },
@@ -85,6 +72,7 @@ export const CreateAd = () => {
           meta: {
             description: 'Job description',
             header: 'Job Title',
+            name: 'description',
           },
         },
       ],
@@ -97,16 +85,102 @@ export const CreateAd = () => {
           meta: {
             description: 'Job description',
             header: 'Job Title',
+            name: 'photo',
           },
         },
       ],
     },
   ]
 
+  const fields = {}
+
+  for (let row of data) {
+    for (let field of row.children) {
+      fields[field.meta.name] = ''
+    }
+  }
+
+  const { formState, setFormState, handleChange, handleSubmit } = useForm(
+    fields
+  )
+
   return (
     <>
       <Reset />
-      <NewAd></NewAd>
+      <NewAd>
+        {data.map(({ children }, key) => {
+          return (
+            <Flex justifyBetween alignCenter key={key}>
+              {children &&
+                children.map((child, key, arr) => {
+                  const { isRequired, meta, children } = child
+
+                  return (
+                    <FlexItem basis={arr.length > 1 ? '50%' : '100%'} key={key}>
+                      <Margin
+                        left={
+                          arr.length > 1 && key === arr.length - 1 ? '4' : '0'
+                        }
+                      >
+                        {child.type === 'text' && (
+                          <InputGroup
+                            isRequired={isRequired}
+                            title={meta.header}
+                            inputText={meta.description}
+                            onChange={handleChange}
+                            value={formState[meta.name]}
+                          />
+                        )}
+                        {child.type === 'number' && (
+                          <InputGroup
+                            isRequired={isRequired}
+                            title={meta.header}
+                            inputText={meta.description}
+                            onChange={handleChange}
+                            value={formState[meta.name]}
+                          />
+                        )}
+                        {child.type === 'description' && (
+                          <TextEditor
+                            isRequired={isRequired}
+                            title={meta.header}
+                            value={formState[meta.name]}
+                            setFormState={setFormState}
+                            name={formState[meta.name]}
+                          />
+                        )}
+                        {child.type === 'photo' && <FileUploader />}
+                        {child.type === 'select' && (
+                          <SelectGroup
+                            isRequired={isRequired}
+                            title={meta.header}
+                            options={children}
+                            inputText={meta.description}
+                            value={formState[meta.name]}
+                            onChange={handleChange}
+                          />
+                        )}
+                        {child.type === 'radio' && (
+                          <RadioGroup
+                            isRequired={isRequired}
+                            title={meta.header}
+                            choices={children}
+                            name={formState[meta.name]}
+                            value={children[0]}
+                            onChange={handleChange}
+                          />
+                        )}
+                      </Margin>
+                    </FlexItem>
+                  )
+                })}
+            </Flex>
+          )
+        })}
+        <Margin top={3}>
+          <Button>Post job listing</Button>
+        </Margin>
+      </NewAd>
     </>
   )
 }
