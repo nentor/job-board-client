9700df06534b464c8f3d03cb54b9d570c0a8b3c2
NentoR <nentormail@gmail.com>
2020-12-28 20:33:17 +0200

    Use '?.' instead of '&&'

--- src/components/CreateAd/index.js
+++ src/components/CreateAd/index.js
@@ -109,68 +109,67 @@ export const CreateAd = () => {
         {data.map(({ children }, key) => {
           return (
             <Flex justifyBetween alignCenter key={key}>
-              {children &&
-                children.map((child, key, arr) => {
-                  const { isRequired, meta, children } = child
+              {children?.map((child, key, arr) => {
+                const { isRequired, meta, children } = child
 
-                  return (
-                    <FlexItem basis={arr.length > 1 ? '50%' : '100%'} key={key}>
-                      <Margin
-                        left={
-                          arr.length > 1 && key === arr.length - 1 ? '4' : '0'
-                        }
-                      >
-                        {(child.type === 'text' || child.type === 'number') && (
-                          <InputGroup
-                            isRequired={isRequired}
-                            title={meta.header}
-                            inputText={meta.description}
-                            onChange={handleChange}
-                            value={formState[meta.name]}
-                          />
-                        )}
-                        {child.type === 'description' && (
-                          <TextEditor
-                            isRequired={isRequired}
-                            title={meta.header}
-                            value={formState[meta.name]}
-                            setFormState={setFormState}
-                            name={formState[meta.name]}
-                            inputText={meta.description}
-                          />
-                        )}
-                        {child.type === 'photo' && (
-                          <FileUploader
-                            title={meta.header}
-                            isRequired={isRequired}
-                            inputText={meta.description}
-                          />
-                        )}
-                        {child.type === 'select' && (
-                          <SelectGroup
-                            isRequired={isRequired}
-                            title={meta.header}
-                            options={children}
-                            inputText={meta.description}
-                            value={formState[meta.name]}
-                            onChange={handleChange}
-                          />
-                        )}
-                        {child.type === 'radio' && (
-                          <RadioGroup
-                            isRequired={isRequired}
-                            title={meta.header}
-                            choices={children}
-                            name={meta.name}
-                            value={formState[meta.name] || children[0]}
-                            onChange={handleChange}
-                            inputText={meta.description}
-                          />
-                        )}
-                      </Margin>
-                    </FlexItem>
-                  )
-                })}
+                return (
+                  <FlexItem basis={arr.length > 1 ? '50%' : '100%'} key={key}>
+                    <Margin
+                      left={
+                        arr.length > 1 && key === arr.length - 1 ? '4' : '0'
+                      }
+                    >
+                      {(child.type === 'text' || child.type === 'number') && (
+                        <InputGroup
+                          isRequired={isRequired}
+                          title={meta.header}
+                          inputText={meta.description}
+                          onChange={handleChange}
+                          value={formState[meta.name]}
+                        />
+                      )}
+                      {child.type === 'description' && (
+                        <TextEditor
+                          isRequired={isRequired}
+                          title={meta.header}
+                          value={formState[meta.name]}
+                          setFormState={setFormState}
+                          name={formState[meta.name]}
+                          inputText={meta.description}
+                        />
+                      )}
+                      {child.type === 'photo' && (
+                        <FileUploader
+                          title={meta.header}
+                          isRequired={isRequired}
+                          inputText={meta.description}
+                        />
+                      )}
+                      {child.type === 'select' && (
+                        <SelectGroup
+                          isRequired={isRequired}
+                          title={meta.header}
+                          options={children}
+                          inputText={meta.description}
+                          value={formState[meta.name]}
+                          onChange={handleChange}
+                        />
+                      )}
+                      {child.type === 'radio' && (
+                        <RadioGroup
+                          isRequired={isRequired}
+                          title={meta.header}
+                          choices={children}
+                          name={meta.name}
+                          value={formState[meta.name] || children[0]}
+                          onChange={handleChange}
+                          inputText={meta.description}
+                        />
+                      )}
+                    </Margin>
+                  </FlexItem>
+                )
+              })}
             </Flex>
           )
         })}
