8aaab0716175087d7777a1315c453c03653a121e
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-12-29 18:50:08 +0200

    code split mock data

--- pages/create.js
+++ pages/create.js
@@ -1,2 +1,89 @@
+import React from 'react'
 import { CreateAd } from '../src/components'
-export default CreateAd
+
+const CreateAdPage = ({ data }) => {
+  return <CreateAd data={data} />
+}
+
+export async function getStaticProps() {
+  const mockData = [
+    {
+      children: [
+        {
+          isRequired: true,
+          type: 'text',
+          meta: {
+            description: 'Job description',
+            header: 'Job Title',
+            name: 'jobTitle',
+          },
+        },
+      ],
+    },
+    {
+      children: [
+        {
+          isRequired: true,
+          type: 'select',
+          meta: {
+            description: 'Job description',
+            header: 'Job Title',
+            name: 'category',
+          },
+          children: [
+            'gosho',
+            'gosho',
+            'gosho',
+            'gosho',
+            'gosho',
+            'gosho',
+            'gosho',
+          ],
+        },
+        {
+          isRequired: true,
+          type: 'radio',
+          meta: {
+            description: 'Job description',
+            header: 'Job Title',
+            name: 'jobType',
+          },
+          children: ['gosho', 'tosho', 'dancho'],
+        },
+      ],
+    },
+    {
+      children: [
+        {
+          isRequired: true,
+          type: 'description',
+          meta: {
+            description: 'Job description',
+            header: 'Job Title',
+            name: 'description',
+          },
+        },
+      ],
+    },
+    {
+      children: [
+        {
+          isRequired: true,
+          type: 'photo',
+          meta: {
+            description: 'Job description',
+            header: 'Job Title',
+            name: 'photo',
+          },
+        },
+      ],
+    },
+  ]
+
+  return {
+    props: {
+      data: mockData,
+    },
+  }
+}
+export default CreateAdPage
